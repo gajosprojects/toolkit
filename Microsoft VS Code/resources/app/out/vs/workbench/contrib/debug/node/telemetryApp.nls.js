659b42099c1e01314fe1afc0c6ddbafd83b03bae
gajosprojects <gajosprojects@gmail.com>
2019-03-11 23:31:18 -0300

    Issue #2
    Issue #2 - Disponibilizar ambiente de programação com as extensões e a ide padronizados

---
+++ Microsoft VS Code/resources/app/out/vs/workbench/contrib/debug/node/telemetryApp.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/workbench/contrib/debug/node/telemetryApp.nls",{"vs/base/node/processes":["Can't execute a shell command on a UNC drive."]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/05f146c7a8f7f78e80261aa3b2a2e642586f9eb3/core/vs\workbench\contrib\debug\node\telemetryApp.nls.js.map
