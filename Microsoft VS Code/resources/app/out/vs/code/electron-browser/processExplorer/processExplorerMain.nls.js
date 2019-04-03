8347bf587e1d4a039bcba860665bb9681c8f9b38
gajosprojects <gajosprojects@gmail.com>
2019-04-02 21:47:31 -0300

    Excluindo Vs Code

--- Microsoft VS Code/resources/app/out/vs/code/electron-browser/processExplorer/processExplorerMain.nls.js
+++
@@ -1,5 +0,0 @@
-/*!--------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-define("vs/code/electron-browser/processExplorer/processExplorerMain.nls",{"vs/code/electron-browser/processExplorer/processExplorerMain":["CPU %","Memory (MB)","pid","Name","Kill Process","Force Kill Process","Copy","Copy All","Debug","Copy All"]});
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/05f146c7a8f7f78e80261aa3b2a2e642586f9eb3/core/vs\code\electron-browser\processExplorer\processExplorerMain.nls.js.map
--- Microsoft VS Code/resources/app/out/vs/code/electron-browser/sharedProcess/sharedProcess.js
+++
@@ -1,6 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-"use strict";const bootstrap=require("../../../../bootstrap"),bootstrapWindow=require("../../../../bootstrap-window");bootstrap.avoidMonkeyPatchFromAppInsights(),bootstrapWindow.load(["vs/code/electron-browser/sharedProcess/sharedProcessMain"],function(o,t){o.startup({machineId:t.machineId})});
-//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/05f146c7a8f7f78e80261aa3b2a2e642586f9eb3/core/vs\code\electron-browser\sharedProcess\sharedProcess.js.map
